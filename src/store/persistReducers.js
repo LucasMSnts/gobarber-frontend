6fe682cc775bd00a8674e637b6249e9c769e2196
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-11-12 17:21:23 -0300

    Loading da autenticação

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -3,12 +3,12 @@ import { persistReducer } from 'redux-persist';
 
 export default reducers => {
     const persistedReducer = persistReducer(
-      {
-        key: 'gobarber',
-        storage,
-        whitelist: ['auth', 'user'],
-      },
-      reducers
+        {
+            key: 'gobarber',
+            storage,
+            whitelist: ['auth', 'user'],
+        },
+        reducers
     );
 
     return persistedReducer;
